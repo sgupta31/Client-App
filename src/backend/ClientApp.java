0f1b66b300a760ef86dcb0646b22d88a8798997f
anitaszilagyi <anita.szilagyi@mail.mcgill.ca>
2014-02-27 16:25:52 -0500

    initial setup

---
+++ src/backend/ClientApp.java
@@ -0,0 +1,26 @@
+package backend;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class ClientApp {
+
+	String host = "dsp2014.ece.mcgill.ca";
+	int port = 5000;
+
+	public void connectToServer() {
+
+		try {
+			InetAddress address = InetAddress.getByName(host);
+			Socket clientConnection = new Socket (address, port);
+		} catch (IOException e) {
+			System.err.println("IOException: " + e);
+		} catch (Exception exc) {
+			System.err.println("Exception: " + exc);
+		}
+
+	}
+
+
+}
