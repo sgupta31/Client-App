c6f625a00ac2e25daf3e4c5c136973b46a6e5389
anitaszilagyi <anita.szilagyi@mail.mcgill.ca>
2014-02-28 17:59:52 -0500

    login and register

--- src/UI/ClientApp.java
+++ src/UI/ClientApp.java
@@ -4,11 +4,31 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
 import java.awt.BorderLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+
+import backend.TelecomClient;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
+import javax.swing.JTextField;
 
 public class ClientApp {
 
-	private JFrame frame;
+	private static JFrame frame;
+	public static JButton btnLogin;
+	private static RegistrationFrame regFrame;
+	private JLabel lblUsername;
+	private JTextField username;
+	private JLabel lblPassword;
+	private JTextField password;
 
 	/**
 	 * Launch the application.
@@ -17,8 +37,10 @@ public class ClientApp {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					TelecomClient.connectToServer();
 					ClientApp window = new ClientApp();
 					window.frame.setVisible(true);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -37,12 +59,66 @@ public class ClientApp {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JLabel lblHello = new JLabel("Hello");
-		frame.getContentPane().add(lblHello, BorderLayout.NORTH);
-	}
+		btnLogin = new JButton("Login");
+		btnLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				
+				TelecomClient.loginUser(username.getText(), password.getText());
+				
+			}
+		});
+		btnLogin.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		btnLogin.setBounds(100, 191, 106, 36);
+		frame.getContentPane().add(btnLogin);
+		
+		JButton btnRegister = new JButton("Register");
+		btnRegister.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
 
+//				frame.setEnabled(t);
+				regFrame = new RegistrationFrame();
+				regFrame.setVisible(true);
+				//btnLogin.setEnabled(false);
+				
+			}
+		});
+		btnRegister.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		btnRegister.setBounds(259, 191, 111, 36);
+		frame.getContentPane().add(btnRegister);
+		
+		JLabel lblChatClient = new JLabel("Chat Client");
+		lblChatClient.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		lblChatClient.setBounds(172, 6, 118, 60);
+		frame.getContentPane().add(lblChatClient);
+		
+		lblUsername = new JLabel("Username:");
+		lblUsername.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		lblUsername.setBounds(74, 78, 102, 36);
+		frame.getContentPane().add(lblUsername);
+		
+		username = new JTextField();
+		username.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		username.setBounds(259, 83, 134, 28);
+		frame.getContentPane().add(username);
+		username.setColumns(10);
+		
+		lblPassword = new JLabel("Password:");
+		lblPassword.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		lblPassword.setBounds(74, 144, 91, 23);
+		frame.getContentPane().add(lblPassword);
+		
+		password = new JTextField();
+		password.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		password.setBounds(259, 142, 134, 28);
+		frame.getContentPane().add(password);
+		password.setColumns(10);
+	}
 }
--- src/backend/TelecomClient.java
+++ src/backend/TelecomClient.java
@@ -1,25 +1,86 @@
 package backend;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 
 public class TelecomClient {
 
-	String host = "dsp2014.ece.mcgill.ca";
-	int port = 5000;
+	static String host = "dsp2014.ece.mcgill.ca";
+	static int port = 5000;
+	static Socket clientConnection;
+	static InputStream in;
+	static BufferedWriter out;
+	
+	public static int registerUser(String username, String password) {
+		
+		String payload;
+		
+		payload = username + "," + password;
+		
+		byte[] msgType = ByteBuffer.allocate(4).putInt(5).array();
+		byte[] subMsgType = ByteBuffer.allocate(4).putInt(0).array();
+		byte[] msgData = payload.getBytes();
+		byte[] size = ByteBuffer.allocate(4).putInt(msgData.length).array();
+		
+		byte[] msg = createMessage(msgType, subMsgType, size, msgData);
+		
+		
+		
+		return 0;
+		
+	}
+	
+	public static int loginUser(String username, String password) {
+		
+		String payload;
+		
+		payload = username + "," + password;
+		
+		byte[] msgType = ByteBuffer.allocate(4).putInt(5).array();
+		byte[] subMsgType = ByteBuffer.allocate(4).putInt(0).array();
+		byte[] msgData = payload.getBytes();
+		byte[] size = ByteBuffer.allocate(4).putInt(msgData.length).array();
+		
+		byte[] msg = createMessage(msgType, subMsgType, size, msgData);
+		
+		return 0;
+	}
 
-	public void connectToServer() {
+	public static byte[] createMessage(byte[] msgType, byte[] subMsgType, byte[] size, byte[] msgData) {
+		
+		byte[] msg = new byte[4*3 + msgData.length];
+		
+		System.arraycopy(msgType, 0, msg, 0, 4);
+		System.arraycopy(subMsgType, 0, msg, 4, 4);
+		System.arraycopy(size, 0, msg, 8, 4);
+		System.arraycopy(msgData, 0, msg, 12, msgData.length);
+		
+		return msg;
+	}
+	
+	public static void connectToServer() throws IOException {
 
 		try {
 			InetAddress address = InetAddress.getByName(host);
-			Socket clientConnection = new Socket (address, port);
+			clientConnection = new Socket (address, port);
+			//out = new PrintWriter(clientConnection.getOutputStream(), true);
+			//in = yourSocketHere.getInputStream();
 		} catch (IOException e) {
 			System.err.println("IOException: " + e);
 		} catch (Exception exc) {
 			System.err.println("Exception: " + exc);
 		}
-
+	}
+	
+	public static void closeSocket() throws Exception {
+		clientConnection.close();
+		System.out.println("hhrhrhg");
 	}
 
 
